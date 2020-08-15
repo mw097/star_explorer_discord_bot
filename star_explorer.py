b47823d0ac1c4fdb3c78fd689f1db22caf31ca31
Michał Węgrzyn <mihelone@gmail.com>
2020-08-15 18:22:34 +0200

    Add help comamnd

--- star_explorer.py
+++ star_explorer.py
@@ -24,14 +24,41 @@ class star_explorer(discord.Client):
         """Method called when new message is received by bot"""
 
         # Check if sender is self
-        if message.author.id != self.user.id:
+        if message.author.id == self.user.id:
             return
 
         # Check if has command prefix
         if not message.content.startswith(settings.command_prefix):
             return
 
-        # TODO: Parse commands
+        # Parse command and args
+        command = message.content.replace(settings.command_prefix, "", 1).split(" ")[0]
+        args = message.content.replace(settings.command_prefix, "", 1).split(" ")[1:]
 
+        # ## Execute command ## #
+
+        # help
+        if command == "help":
+            await self.command_help(message)
+            await message.delete()
+
+        # TODO
+        # help <args>
+        if command == "help" and len(args) == 1:
+            await self.command_help(args, message.channel)
+            await message.delete()
+
+    # ## Commands ## #
+
+    async def command_help(self, message):
+        """Shows help"""
+
+        await message.channel.send('Use command like this: $<command_name> <arguments>. For example: $apod')
+
+    # TODO
+    async def command_help_ex(self, message, args):
+        """Shows extended help"""
+
+        pass
 
 star_explorer().run_bot()
